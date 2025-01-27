ce248e6c3ed963412fd40d6a1e79dbdad66a6c16
Ali Usman <mrau429040@gmail.com>
2025-01-27 18:59:29 +0500

    Basic code setup

---
+++ OnlineExamSystem.java
@@ -0,0 +1,14 @@
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.*;
+import java.util.ArrayList;
+import java.util.List;
+
+public class OnlineExamSystem {
+    
+}
+
+// Question class
+class Question {
+    
+}
