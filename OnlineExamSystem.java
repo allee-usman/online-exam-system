b2d074e742aac8ddb629c0fa57242c12feadf4ef
Ali Usman <mrau429040@gmail.com>
2025-01-27 21:24:28 +0500

    question initialize code added

--- OnlineExamSystem.java
+++ OnlineExamSystem.java
@@ -5,7 +5,17 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class OnlineExamSystem {
-    
+    private static List<Question> questionBank = new ArrayList<>();
+
+    // Initialize the question bank with sample questions
+    private static void initializeQuestionBank() {
+        questionBank.add(new Question("Which keyword is used to define a class in Java?", new String[]{"class", "Class", "struct", "def"}, 1));
+        questionBank.add(new Question("Which data type is used to store decimal numbers in Java?", new String[]{"int", "float", "char", "boolean"}, 2));
+        questionBank.add(new Question("What is the default value of a boolean variable in Java?", new String[]{"true", "false", "null", "undefined"}, 2));
+        questionBank.add(new Question("Which of the following is NOT a valid access modifier in Java?", new String[]{"public", "protected", "private", "defaulted"}, 4));
+    }
+
+
 }
 
 // Question class
