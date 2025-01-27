617b316072edb818b49b9ee0c60c85ac99add1d5
Ali Usman <mrau429040@gmail.com>
2025-01-27 21:31:43 +0500

    More question added

--- OnlineExamSystem.java
+++ OnlineExamSystem.java
@@ -13,6 +13,12 @@ public class OnlineExamSystem {
         questionBank.add(new Question("Which data type is used to store decimal numbers in Java?", new String[]{"int", "float", "char", "boolean"}, 2));
         questionBank.add(new Question("What is the default value of a boolean variable in Java?", new String[]{"true", "false", "null", "undefined"}, 2));
         questionBank.add(new Question("Which of the following is NOT a valid access modifier in Java?", new String[]{"public", "protected", "private", "defaulted"}, 4));
+        questionBank.add(new Question("Which of the following is a valid way to start the main method in Java?", new String[]{"public static void main(String[] args)", "void main(String[] args)", "static void main()", "main()"}, 1));
+        questionBank.add(new Question("What is the size of an 'int' data type in Java?", new String[]{"16 bits", "32 bits", "64 bits", "8 bits"}, 2));
+        questionBank.add(new Question("Which method is used to print something on the console in Java?", new String[]{"System.output()", "System.print()", "System.out.println()", "System.console()"}, 3));
+        questionBank.add(new Question("Which exception is thrown when dividing by zero in Java?", new String[]{"ArithmeticException", "NullPointerException", "NumberFormatException", "IOException"}, 1));
+        questionBank.add(new Question("What is the keyword used to inherit a class in Java?", new String[]{"implement", "inherits", "extends", "super"}, 3));
+        questionBank.add(new Question("Which package is automatically imported in every Java program?", new String[]{"java.util", "java.io", "java.lang", "java.net"}, 3));
     }
 
 
